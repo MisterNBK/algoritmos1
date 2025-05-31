e9f6a7aa1d9c9d7d0e8b20ac7270b03e8a84fe32
MisterNBK <viniciosvelasques@gmail.com>
2025-05-31 18:21:11 -0300

    atv6.c
    concertado

--- atv6.c
+++ atv6.c
@@ -1,38 +1,55 @@
 #include <stdio.h>
 
-    int main() {
-        int matriz[100][100];
-        int matriz2[100][100];
-        int produto[100][100];
-        int linha, coluna, i, j;
+int main() {
+    int matriz[100][100];
+    int matriz2[100][100];
+    int produto[100][100];
+    int linha1, coluna1, linha2, coluna2;
+    int i, j, k;
 
-        printf("quantas linhas e colunas nas matrizes: ");
-        scanf("%d %d", &linha, &coluna);
+    printf("quantas linhas e colunas matriz1: ");
+    scanf("%d %d", &linha1, &coluna1);
+    printf("quantas linhas e colunas matriz2: ");
+    scanf("%d %d", &linha2, &coluna2);
 
-        
-        for(i = 0; i < linha; i++){
-            for(j = 0; j < coluna; j++){
-                scanf("%d", &matriz[i][j]); 
-            }
+    if (coluna1 != linha2) {
+        printf("erro\n");
+        return 1;
+    }
+    printf("matriz 1:\n");
+    for (i = 0; i < linha1; i++) {
+        for (j = 0; j < coluna1; j++) {
+            scanf("%d", &matriz[i][j]);
         }
-        
-        for(i = 0; i < linha; i++){
-            for(j = 0; j < coluna; j++){
-                scanf("%d", &matriz2[i][j]); 
-            }
-        }
-       
-        for(i = 0; i < linha; i++){
-            for(j = 0; j < coluna; j++){
-                produto[i][j] = matriz[i][j] * matriz2[i][j];
-            }
-        }
+    }
 
-        for(i = 0; i < linha; i++){
-            for(j = 0; j < coluna; j++){
-                printf("%d ", produto[i][j]);
+    printf("matriz 2:\n");
+    for (i = 0; i < linha2; i++) {
+        for (j = 0; j < coluna2; j++) {
+            scanf("%d", &matriz2[i][j]);
+        }
+    }
+
+    for (i = 0; i < linha1; i++) {
+        for (j = 0; j < coluna2; j++) {
+            produto[i][j] = 0;
+        }
+    }
+
+    for (i = 0; i < linha1; i++) {
+        for (j = 0; j < coluna2; j++) {
+            for (k = 0; k < coluna1; k++) {
+                produto[i][j] += matriz[i][k] * matriz2[k][j];
             }
         }
+    }
+
+    for (i = 0; i < linha1; i++) {
+        for (j = 0; j < coluna2; j++) {
+            printf("%d ", produto[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
